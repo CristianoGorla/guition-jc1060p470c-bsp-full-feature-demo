2a3ab24bbcd3fa26aa9a2e28bf9c5f9d38b13b44
Cristiano Gorla <cristiano.gorla@gmail.com>
2026-03-01 17:56:24 +0100

    Revert to correct GT911 pins: GPIO21=INT, GPIO22=RST (verified from schematic)

--- main/touch_gt911.c
+++ main/touch_gt911.c
@@ -10,8 +10,9 @@
 static const char *TAG = "GT911";
 
 // Pin definitions from schematic (Guition JC1060P470C_I_W_Y)
-#define GT911_RST_GPIO         GPIO_NUM_19  // TOUCHRST
-#define GT911_INT_GPIO         GPIO_NUM_20  // TOUCHINT
+// Verified from complete schematic: FPC3 connector pins
+#define GT911_RST_GPIO         GPIO_NUM_22  // TOUCHRST
+#define GT911_INT_GPIO         GPIO_NUM_21  // TOUCHINT
 #define GT911_I2C_ADDRESS      0x14  // Address with INT=HIGH during reset
 
 esp_lcd_touch_handle_t init_touch_gt911(i2c_master_bus_handle_t i2c_bus)
