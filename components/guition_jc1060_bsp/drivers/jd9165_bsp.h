c8b795049a002a4b0a4d879834d25f16aaf95016
Cristiano Gorla <cristiano.gorla@gmail.com>
2026-03-02 18:18:21 +0100

    feat: Add display driver (JD9165) to BSP component with Kconfig integration

---
+++ components/guition_jc1060_bsp/drivers/jd9165_bsp.h
@@ -0,0 +1,45 @@
+/**
+ * @file jd9165_bsp.h
+ * @brief JD9165 MIPI-DSI Display Driver for BSP
+ * 
+ * Hardware Configuration:
+ * - Display: 1024x600 RGB565
+ * - Interface: MIPI DSI 2-lane @ 750 Mbps
+ * - Pixel Clock: 51.2 MHz (effective)
+ * - Backlight: PWM on GPIO 23
+ * - Reset: GPIO 0
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "esp_lcd_panel_ops.h"
+#include "esp_err.h"
+
+/**
+ * @brief Initialize JD9165 display driver
+ * 
+ * This function initializes the complete display subsystem:
+ * - MIPI DSI PHY power (LDO channel 3 @ 2.5V)
+ * - 2-lane DSI bus @ 750 Mbps
+ * - Display panel with init sequence
+ * - Backlight PWM controller
+ * 
+ * @return LCD panel handle on success, NULL on failure
+ */
+esp_lcd_panel_handle_t bsp_display_init(void);
+
+/**
+ * @brief Set display backlight brightness
+ * 
+ * @param brightness_percent Brightness level 0-100%
+ * @return ESP_OK on success
+ */
+esp_err_t bsp_display_set_brightness(uint8_t brightness_percent);
+
+#ifdef __cplusplus
+}
+#endif
